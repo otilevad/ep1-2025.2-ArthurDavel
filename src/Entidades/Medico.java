4f7b062cf7e6016bc99da6434fca2560bd235ca2
otilevad <arthurdavelvb@gmail.com>
2025-09-15 13:31:54 -0300

    Construtores, getters e setters doe Medico e Paciente

--- src/Entidades/Medico.java
+++ src/Entidades/Medico.java
@@ -1,7 +1,24 @@
 package Entidades;
 public class Medico extends Pessoa{
     private String crm;
-    private int idade;
+
+    public Medico(){
+        super();
+        this.crm="";
+    }
+
+    public Medico(String nome, String crm){
+        super();
+        this.crm=crm;
+    }
+
+    public String getCrm() {
+        return this.crm;
+    }
+
+    public void setCrm(String crm) {
+        this.crm=crm;
+    }
 
     @Override
     public void imprimeDados(){
--- src/Entidades/Paciente.java
+++ src/Entidades/Paciente.java
@@ -3,6 +3,34 @@ public class Paciente extends Pessoa{
     private String cpf;
     private int idade;
 
+    public Paciente(){
+        super();
+        this.cpf="";
+        this.idade=0;
+    }
+
+    public Paciente(String nome, String cpf, int idade){
+        super();
+        this.cpf=cpf;
+        this.idade=idade;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf=cpf;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade=idade;
+    }
+
     @Override
     public void imprimeDados(){
 
