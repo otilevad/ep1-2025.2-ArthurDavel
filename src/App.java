658815428a8d50325fa83f7c111b8530dec775cb
otilevad <arthurdavelvb@gmail.com>
2025-09-22 12:59:45 -0300

    Pequena correção na main acerca dos inputs inválidos

--- src/App.java
+++ src/App.java
@@ -1,6 +1,5 @@
 import Menu.*;
 import Utilitarios.*;
-
 import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Locale;
@@ -14,16 +13,16 @@ public class App {
         Lista listas = new Lista();
         int menuSelecionado=0; //id do menu
         int opt=0;
-        Menu menuAtual=new Menu();
+        Menu menuAtual=menus.get(MenuSetup.procuraMenu(menus, menuSelecionado));
         boolean skipInput=false;
-        mainLoop: while(true){
+        while(true){
             Misc.limpaTela();
             if(opt>=0){
                 menuSelecionado=opt;
             }
             else if(opt==-2){
                 System.out.println("Saindo...");
-                break mainLoop;
+                break;
             }
             else if(opt==-1){
                 System.out.println("Por favor, digite uma opção válida.");
@@ -35,21 +34,20 @@ public class App {
             if(!skipInput){
                 menuAtual=menus.get(MenuSetup.procuraMenu(menus, menuSelecionado)); //ordem do menu no arraylist
                 menuAtual.mostraMenu(menuAtual.getOpts());
-                opt=-1;
                 try{
                     opt = sc.nextInt();
                     sc.nextLine();
                 }
                 catch(InputMismatchException e){
                     sc.next();
+                    opt=-1;
                 }
                 if(opt>=0 && opt<menuAtual.getOpts().size()){
                     opt=menuAtual.getOpts().get(opt).getDestino();
                 }
+                else{opt=-1;}
             }
-            else{
-                skipInput=false;
-            }
+            else{skipInput=false;}
         }
         sc.close();
     }
